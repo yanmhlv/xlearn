690e15a801161164e4e0d09ec2b15e4b733b39de
Ian Mikhailov <1557291+yanmhlv@users.noreply.github.com>
2026-08-11 14:10:42 -0300

    resolve the stale and unimplemented core TODOs

--- src/data/data_structure.h
+++ src/data/data_structure.h
@@ -130,7 +130,6 @@ typedef std::vector<Node> SparseRow;
 //    index_t max_feat = matrix.MaxFeat();
 //    index_t max_field = matrix.MaxField();
 //------------------------------------------------------------------------------
-// TODO(aksnzhy): Implement incremental adding
 struct DMatrix {
   // Constructor
   DMatrix()
